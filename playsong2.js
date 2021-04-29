3b32b51e342dbbc833108295a11cd2e6ba293bf5
Zoe Khan <zoekhan@seattleacademy.org>
2021-04-29 13:21:00 -0700

    Update songs

---
+++ playsong2.js
@@ -0,0 +1,60 @@
+function playSong2(){
+	var piano = Synth.createInstrument('piano');
+	//measure 1
+	setTimeout(function(){piano.play("C#",4,1)},0);
+	setTimeout(function(){piano.play("E",4,1)},150);
+	setTimeout(function(){piano.play("A",4,1)},300);
+	setTimeout(function(){piano.play("C#",5,1)},450);
+	setTimeout(function(){piano.play("E",5,1)},600);
+	setTimeout(function(){piano.play("F#",5,1)},750);
+	setTimeout(function(){piano.play("G#",5,1)},900);
+	setTimeout(function(){piano.play("D#",5,1)},1050);
+	setTimeout(function(){piano.play("B",4,1)},1200);
+	setTimeout(function(){piano.play("G#",4,1)},1350);
+	setTimeout(function(){piano.play("D#",4,1)},1500);
+	setTimeout(function(){piano.play("B",3,1)},1650);
+	//measure 2
+	setTimeout(function(){piano.play("A",3,1)},1800);
+	setTimeout(function(){piano.play("C#",4,1)},1950);
+	setTimeout(function(){piano.play("F#",4,1)},2100);
+	setTimeout(function(){piano.play("A",4,1)},2250);
+	setTimeout(function(){piano.play("C#",5,1)},2400);
+	setTimeout(function(){piano.play("D#",5,1)},2550);
+	setTimeout(function(){piano.play("E",5,1)},2700);
+	setTimeout(function(){piano.play("B",4,1)},2850);
+	setTimeout(function(){piano.play("G#",4,1)},3000);
+	setTimeout(function(){piano.play("E",4,1)},3150);
+	setTimeout(function(){piano.play("B",3,1)},3300);
+	setTimeout(function(){piano.play("A",3,1)},3450);
+	//measure 3
+	setTimeout(function(){piano.play("F#",3,1.5)},3600);
+	setTimeout(function(){piano.play("A",3,1)},3750);
+	setTimeout(function(){piano.play("C#",4,1.5)},3900);
+	setTimeout(function(){piano.play("C#",5,1)},4050);
+	setTimeout(function(){piano.play("F#",4,1)},4200);
+	setTimeout(function(){piano.play("A",4,1.5)},4350);
+	setTimeout(function(){piano.play("F#",5,1)},4500);
+	setTimeout(function(){piano.play("E",4,1.5)},4500);
+	setTimeout(function(){piano.play("A",4,1)},4650);
+	setTimeout(function(){piano.play("C#",4,1.5)},4800);
+	setTimeout(function(){piano.play("C#",5,1)},4950);
+	setTimeout(function(){piano.play("F#",4,1)},5100);
+	setTimeout(function(){piano.play("A",4,1.5)},5250);
+	//measure 4
+	setTimeout(function(){piano.play("A",4,1)},5400);
+	setTimeout(function(){piano.play("C#",4,1)},5400);
+	setTimeout(function(){piano.play("D#",3,1.5)},5400);
+	setTimeout(function(){piano.play("A",3,1)},5550);
+	setTimeout(function(){piano.play("C#",4,1.5)},5700);
+	setTimeout(function(){piano.play("C#",5,1)},5850);
+	setTimeout(function(){piano.play("F#",4,1)},6000);
+	setTimeout(function(){piano.play("A",4,1.5)},6150);
+	setTimeout(function(){piano.play("F#",5,1)},6150);
+	setTimeout(function(){piano.play("C#",3,1.5)},6150);
+	setTimeout(function(){piano.play("A",3,1)},6300);
+	setTimeout(function(){piano.play("E",4,1.5)},6450);
+	setTimeout(function(){piano.play("A",5,2)},6600);
+	setTimeout(function(){piano.play("A",4,1)},6750);
+	setTimeout(function(){piano.play("C#",4,1)},6900);
+
+}
