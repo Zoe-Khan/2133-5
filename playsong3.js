f3b0459db5c3b4555422eaf00a2d14b98924f677
Zoe Khan <zoekhan@seattleacademy.org>
2021-04-30 17:02:16 -0700

    Update songs

--- playsong3.js
+++ playsong3.js
@@ -1,39 +1,191 @@
-function playSong2(){
-
+function playSong3(){
 	var piano = Synth.createInstrument('piano');
-	setTimeout(function() {piano.play('E', 3, 1)},0);
-	setTimeout(function() {piano.play('E', 3, 1)},400);
-	setTimeout(function() {piano.play('F', 3, 1)},800);
-	setTimeout(function() {piano.play('G', 3, 1)},1200);
-	setTimeout(function() {piano.play('G', 3, 1)},1600);
-	setTimeout(function() {piano.play('F', 3, 1)},2000);
-	setTimeout(function() {piano.play('E', 3, 1)},2400);
-	setTimeout(function() {piano.play('D', 3, 1)},2800);
-	setTimeout(function() {piano.play('C', 3, 1)},3200);
-	setTimeout(function() {piano.play('C', 3, 1)},3600);
-	setTimeout(function() {piano.play('D', 3, 1)},4000);
-	setTimeout(function() {piano.play('E', 3, 1)},4400);
-	setTimeout(function() {piano.play('E', 3, 1)},4800);
-	setTimeout(function() {piano.play('D', 3, 1)},5400);
-	setTimeout(function() {piano.play('D', 3, 1)},5600);
+	//measure 1
+	setTimeout(function(){piano.play("C",4,1)},0);
+	setTimeout(function(){piano.play("E",4,1)},200);
+	setTimeout(function(){piano.play("A",4,1)},400);
+	setTimeout(function(){piano.play("C",4,2)},600);
+	setTimeout(function(){piano.play("E",4,1)},1000);
+	setTimeout(function(){piano.play("A",4,1)},1200);
+	setTimeout(function(){piano.play("E",4,1)},1400);
+	//measure 2
+	setTimeout(function(){piano.play("B",3,1)},1600);
+	setTimeout(function(){piano.play("E",4,1)},1800);
+	setTimeout(function(){piano.play("A",4,1)},2000);
+	setTimeout(function(){piano.play("B",3,2)},2200);
+	setTimeout(function(){piano.play("E",4,1)},2600);
+	setTimeout(function(){piano.play("G",4,1)},2800);
+	setTimeout(function(){piano.play("E",4,2)},3000);
+	//measure 3
+	setTimeout(function(){piano.play("C",4,1)},3200);
+	setTimeout(function(){piano.play("A",3,6)},3200);
+	setTimeout(function(){piano.play("A",2,6)},3200);
+	setTimeout(function(){piano.play("E",4,1)},3400);
+	setTimeout(function(){piano.play("A",4,1)},3600);
+	setTimeout(function(){piano.play("C",4,2)},3800);
+	setTimeout(function(){piano.play("E",4,1)},4200);
+	setTimeout(function(){piano.play("A",4,1)},4400);
+	setTimeout(function(){piano.play("B",3,3)},4400);
+	setTimeout(function(){piano.play("B",2,3)},4400);
+	setTimeout(function(){piano.play("E",4,2)},4600);
+	//measure 4
+	setTimeout(function(){piano.play("B",3,1)},4800);
+	setTimeout(function(){piano.play("E",3,6)},4800);
+	setTimeout(function(){piano.play("E",2,6)},4800);
+	setTimeout(function(){piano.play("E",4,1)},5000);
+	setTimeout(function(){piano.play("A",4,1)},5200);
+	setTimeout(function(){piano.play("B",3,2)},5400);
+	setTimeout(function(){piano.play("E",4,1)},5800);
+	setTimeout(function(){piano.play("G",4,1)},6000);
+	setTimeout(function(){piano.play("E",3,3)},6000);
+	setTimeout(function(){piano.play("E",2,3)},6000);
+	setTimeout(function(){piano.play("E",4,1)},6200);
+	//measure 5
+	setTimeout(function(){piano.play("C",4,1)},6400);
+	setTimeout(function(){piano.play("A",3,6)},6400);
+	setTimeout(function(){piano.play("A",2,6)},6400);
+	setTimeout(function(){piano.play("E",4,1)},6600);
+	setTimeout(function(){piano.play("A",4,1)},6800);
+	setTimeout(function(){piano.play("C",4,2)},7000);
+	setTimeout(function(){piano.play("E",4,1)},7400);
+	setTimeout(function(){piano.play("A",4,1)},7600);
+	setTimeout(function(){piano.play("B",3,3)},7600);
+	setTimeout(function(){piano.play("B",2,3)},7600);
+	setTimeout(function(){piano.play("E",4,2)},7800);
+	//measure 6
+	setTimeout(function(){piano.play("B",3,1)},8000);
+	setTimeout(function(){piano.play("E",3,6)},8000);
+	setTimeout(function(){piano.play("E",2,6)},8000);
+	setTimeout(function(){piano.play("E",4,1)},8200);
+	setTimeout(function(){piano.play("A",4,1)},8400);
+	setTimeout(function(){piano.play("B",3,2)},8600);
+	setTimeout(function(){piano.play("E",4,1)},9000);
+	setTimeout(function(){piano.play("G",4,1)},9200);
+	setTimeout(function(){piano.play("E",3,3)},9200);
+	setTimeout(function(){piano.play("E",2,3)},9200);
+	setTimeout(function(){piano.play("E",4,1)},9400);
+	//measure 7
+	setTimeout(function(){piano.play("C",4,1)},9600);
+	 setTimeout(function(){piano.play("E",4,1)},9600);
+	 setTimeout(function(){piano.play("A",4,1)},9600);
+	setTimeout(function(){piano.play("C",4,1)},9800);
+	 setTimeout(function(){piano.play("E",4,1)},9800);
+	 setTimeout(function(){piano.play("A",4,1)},9800);
+	setTimeout(function(){piano.play("C",4,1)},10000);
+	 setTimeout(function(){piano.play("E",4,1)},10000);
+	 setTimeout(function(){piano.play("A",4,1)},10000);
+	setTimeout(function(){piano.play("C",4,1)},10200);
+	 setTimeout(function(){piano.play("E",4,1)},10200);
+	 setTimeout(function(){piano.play("A",4,1)},10200);
 
-	setTimeout(function() {piano.play('E', 3, 1)},6400);
-	setTimeout(function() {piano.play('E', 3, 1)},6800);
-	setTimeout(function() {piano.play('F', 3, 1)},7200);
-	setTimeout(function() {piano.play('G', 3, 1)},7600);
-	setTimeout(function() {piano.play('G', 3, 1)},8000);
-	setTimeout(function() {piano.play('F', 3, 1)},8400);
-	setTimeout(function() {piano.play('E', 3, 1)},8800);
-	setTimeout(function() {piano.play('D', 3, 1)},9200);
-	setTimeout(function() {piano.play('C', 3, 1)},9600);
-	setTimeout(function() {piano.play('C', 3, 1)},10000);
+	 setTimeout(function(){piano.play("C",4,1)},10400);
+	 setTimeout(function(){piano.play("E",4,1)},10400);
+	 setTimeout(function(){piano.play("A",4,1)},10400);
+	setTimeout(function(){piano.play("C",4,1)},10600);
+	 setTimeout(function(){piano.play("E",4,1)},10600);
+	 setTimeout(function(){piano.play("A",4,1)},10600);
+	setTimeout(function(){piano.play("C",4,1)},10800);
+	 setTimeout(function(){piano.play("E",4,1)},10800);
+	 setTimeout(function(){piano.play("A",4,1)},10800);
+	setTimeout(function(){piano.play("C",4,1)},11000);
+	 setTimeout(function(){piano.play("E",4,1)},11000);
+	 setTimeout(function(){piano.play("A",4,1)},11000);
 
-	setTimeout(function() {piano.play('D', 3, 1)},10400);
-	setTimeout(function() {piano.play('E', 3, 1)},10800);
-	setTimeout(function() {piano.play('D', 3, 1)},11200);
-	setTimeout(function() {piano.play('C', 3, 1)},11800);
-	setTimeout(function() {piano.play('C', 3, 1)},12000);
+	setTimeout(function(){piano.play("A",3,6)},9600);
+	setTimeout(function(){piano.play("A",2,6)},9600);
+	setTimeout(function(){piano.play("B",3,3)},10800);
+	setTimeout(function(){piano.play("B",2,3)},10800);
+	//measure 8
+	setTimeout(function(){piano.play("B",3,1)},11200);
+	 setTimeout(function(){piano.play("E",4,1)},11200);
+	 setTimeout(function(){piano.play("A",4,1)},11200);
+	setTimeout(function(){piano.play("B",3,1)},11400);
+	 setTimeout(function(){piano.play("E",4,1)},11400);
+	 setTimeout(function(){piano.play("A",4,1)},11400);
+	setTimeout(function(){piano.play("B",3,1)},11600);
+	 setTimeout(function(){piano.play("E",4,1)},11600);
+	 setTimeout(function(){piano.play("A",4,1)},11600);
 
+	setTimeout(function(){piano.play("B",3,1)},11800);
+	 setTimeout(function(){piano.play("E",4,1)},11800);
+	 setTimeout(function(){piano.play("G",4,1)},11800);
+	setTimeout(function(){piano.play("B",3,1)},12000);
+	 setTimeout(function(){piano.play("E",4,1)},12000);
+	 setTimeout(function(){piano.play("G",4,1)},12000);
+	setTimeout(function(){piano.play("B",3,1)},12200);
+	 setTimeout(function(){piano.play("E",4,1)},12200);
+	 setTimeout(function(){piano.play("G",4,1)},12200);
+	setTimeout(function(){piano.play("B",3,1)},12400);
+	 setTimeout(function(){piano.play("E",4,1)},12400);
+	 setTimeout(function(){piano.play("G",4,1)},12400);
+	setTimeout(function(){piano.play("B",3,1)},12600);
+	 setTimeout(function(){piano.play("E",4,1)},12600);
+	 setTimeout(function(){piano.play("G",4,1)},12600);
 
+	setTimeout(function(){piano.play("E",3,3)},11200);
+	setTimeout(function(){piano.play("E",2,3)},11200);
+	setTimeout(function(){piano.play("E",3,3)},12400);
+	setTimeout(function(){piano.play("E",2,3)},12400);
+	//measure 9
+	setTimeout(function(){piano.play("C",4,1)},12600);
+	 setTimeout(function(){piano.play("E",4,1)},12600);
+	 setTimeout(function(){piano.play("A",4,1)},12600);
+	setTimeout(function(){piano.play("C",4,1)},12800);
+	 setTimeout(function(){piano.play("E",4,1)},12800);
+	 setTimeout(function(){piano.play("A",4,1)},12800);
+	setTimeout(function(){piano.play("C",4,1)},13000);
+	 setTimeout(function(){piano.play("E",4,1)},13000);
+	 setTimeout(function(){piano.play("A",4,1)},13000);
+	setTimeout(function(){piano.play("C",4,1)},13200);
+	 setTimeout(function(){piano.play("E",4,1)},13200);
+	 setTimeout(function(){piano.play("A",4,1)},13200);
 
-}
+	setTimeout(function(){piano.play("C",4,1)},13400);
+	 setTimeout(function(){piano.play("E",4,1)},13400);
+	 setTimeout(function(){piano.play("A",4,1)},13400);
+	setTimeout(function(){piano.play("C",4,1)},13600);
+	 setTimeout(function(){piano.play("E",4,1)},13600);
+	 setTimeout(function(){piano.play("A",4,1)},13600);
+	setTimeout(function(){piano.play("C",4,1)},13800);
+	 setTimeout(function(){piano.play("E",4,1)},13800);
+	 setTimeout(function(){piano.play("A",4,1)},13800);
+	setTimeout(function(){piano.play("C",4,1)},14000);
+	 setTimeout(function(){piano.play("E",4,1)},14000);
+	 setTimeout(function(){piano.play("A",4,1)},14000);
+
+	setTimeout(function(){piano.play("A",3,6)},12600);
+	setTimeout(function(){piano.play("A",2,6)},12600);
+	setTimeout(function(){piano.play("B",3,3)},13800);
+	setTimeout(function(){piano.play("B",2,3)},13800);
+	//measure 10
+	setTimeout(function(){piano.play("B",3,1)},14000);
+	 setTimeout(function(){piano.play("E",4,1)},14000);
+	 setTimeout(function(){piano.play("A",4,1)},14000);
+	setTimeout(function(){piano.play("B",3,1)},14200);
+	 setTimeout(function(){piano.play("E",4,1)},14200);
+	 setTimeout(function(){piano.play("A",4,1)},14200);
+	setTimeout(function(){piano.play("B",3,1)},14400);
+	 setTimeout(function(){piano.play("E",4,1)},14400);
+	 setTimeout(function(){piano.play("A",4,1)},14400);
+
+	setTimeout(function(){piano.play("B",3,1)},14600);
+	 setTimeout(function(){piano.play("E",4,1)},14600);
+	 setTimeout(function(){piano.play("G",4,1)},14600);
+	setTimeout(function(){piano.play("B",3,1)},14800);
+	 setTimeout(function(){piano.play("E",4,1)},14800);
+	 setTimeout(function(){piano.play("G",4,1)},14800);
+	setTimeout(function(){piano.play("B",3,1)},15000);
+	 setTimeout(function(){piano.play("E",4,1)},15000);
+	 setTimeout(function(){piano.play("G",4,1)},15000);
+	setTimeout(function(){piano.play("B",3,1)},15200);
+	 setTimeout(function(){piano.play("E",4,1)},15200);
+	 setTimeout(function(){piano.play("G",4,1)},15200);
+	setTimeout(function(){piano.play("B",3,1)},15400);
+	 setTimeout(function(){piano.play("E",4,1)},15400);
+	 setTimeout(function(){piano.play("G",4,1)},15400);
+
+	setTimeout(function(){piano.play("E",3,3)},14000);
+	setTimeout(function(){piano.play("E",2,3)},14000);
+	setTimeout(function(){piano.play("E",3,3)},15200);
+	setTimeout(function(){piano.play("E",2,3)},15200);
+	}
